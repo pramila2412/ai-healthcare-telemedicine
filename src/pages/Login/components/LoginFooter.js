7168659d570b898faf833bdbca6c8c4c77b50767
siva253168 <msiva25062000@gmail.com>
2026-06-25 15:19:20 +0530

    signin page

---
+++ src/pages/Login/components/LoginFooter.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginFooter = () => {
+  return (
+    <footer className="max-w-6xl w-full mx-auto text-center py-4 px-2">
+      <p className="login-footer-text text-[11px] font-semibold text-gray-400">
+        © 2026 MediConnect. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default LoginFooter;
